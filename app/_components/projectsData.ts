e14ab013937b22d819cb3860e10529581105424a
dwild8 <dwild8@dxc.com>
2025-04-14 00:10:33 +0100

    Add in project data

--- app/_components/projectsData.ts
+++ app/_components/projectsData.ts
@@ -6,49 +6,51 @@ export type ProjectData = {
     dateDeveloped: Date,
 }
 
+const cdnRootUrl = "https://cdn.jsdelivr.net/gh/dominicwild/portfolio-v4@master/resources"
+
 export const projectsData: ProjectData[] = [
     {
-        image: "https://cdn.discordapp.com/attachments/1361020215860924599/1361020242578641078/snooker3.gif?ex=67fd3c60&is=67fbeae0&hm=024a0323d3fba5169cd04cefe00997fc3b7af0e792397c0e8843414f2eb0cb26&",
+        image: `${cdnRootUrl}/snooker3.gif`,
         descriptionFile: "markdown/projects/pool.md",
         dateDeveloped: new Date(Date.parse("1 Apr 2016")),
     },
     {
-        image: "https://cdn.discordapp.com/attachments/1361020215860924599/1361024026339709138/krooza.gif?ex=67fd3fe6&is=67fbee66&hm=0318a440ea986db645b1e456e20c3fd88e225c8f266d2853957357546ad3259d&",
+        image: `${cdnRootUrl}/krooza-small.gif`,
         descriptionFile: "markdown/projects/krooza.md",
         dateDeveloped: new Date(Date.parse("1 Oct 2016")),
     },
     {
-        image: "/images/placeholder.jpg",
+        image: `${cdnRootUrl}/portfolio-site.png`,
         descriptionFile: "markdown/projects/portfolio-v1.md",
         dateDeveloped: new Date(Date.parse("1 Jul 2019")),
     },
     {
-        image: "/images/placeholder.jpg",
+        image: `${cdnRootUrl}/heart.gif`,
         descriptionFile: "markdown/projects/ar-mobile-app.md",
         dateDeveloped: new Date(Date.parse("1 Oct 2018")),
     },
     {
-        image: "/images/placeholder.jpg",
+        image: `${cdnRootUrl}/microbit.jpg`,
         descriptionFile: "markdown/projects/microbit-mesh.md",
         dateDeveloped: new Date(Date.parse("1 Oct 2017")),
     },
     {
-        image: "/images/placeholder.jpg",
+        image: `${cdnRootUrl}/nlp.png`,
         descriptionFile: "markdown/projects/nlp-language-classifier.md",
         dateDeveloped: new Date(Date.parse("1 Oct 2018")),
     },
     {
-        image: "/images/placeholder.jpg",
+        image: `${cdnRootUrl}/social2.gif`,
         descriptionFile: "markdown/projects/enterprise-social-network.md",
         dateDeveloped: new Date(Date.parse("1 Jan 2019")),
     },
     {
-        image: "/images/placeholder.jpg",
+        image: `${cdnRootUrl}/auction_system.gif`,
         descriptionFile: "markdown/projects/distributed-auction-system.md",
         dateDeveloped: new Date(Date.parse("1 Nov 2017")),
     },
     {
-        image: "/images/placeholder.jpg",
+        image: `${cdnRootUrl}/dht.png`,
         descriptionFile: "markdown/projects/distributed-storage-worker.md",
         dateDeveloped: new Date(Date.parse("1 Nov 2018")),
     }
