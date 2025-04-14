1f2dd591ad926efe5182556c533977a3d67e811d
dwild8 <dwild8@dxc.com>
2025-04-15 00:04:04 +0100

    Add in all github links

--- app/_components/projectsData.ts
+++ app/_components/projectsData.ts
@@ -13,47 +13,55 @@ export const projectsData: ProjectData[] = [
         image: `${cdnRootUrl}/portfolio-site.png`,
         descriptionFile: "markdown/projects/portfolio-v1.md",
         dateDeveloped: new Date(Date.parse("1 Jul 2019")),
-        githubLink: "https://github.com",
+        githubLink: "https://github.com/dominicwild/Portfolio-Site",
     },
     {
         image: `${cdnRootUrl}/snooker3.gif`,
         descriptionFile: "markdown/projects/pool.md",
         dateDeveloped: new Date(Date.parse("1 Apr 2016")),
+        githubLink: "https://github.com/dominicwild/Java-Pool-Game",
     },
     {
         image: `${cdnRootUrl}/krooza-small.gif`,
         descriptionFile: "markdown/projects/krooza.md",
         dateDeveloped: new Date(Date.parse("1 Oct 2016")),
+        githubLink: "https://github.com/dominicwild/Krooza",
     },
     {
         image: `${cdnRootUrl}/heart.gif`,
         descriptionFile: "markdown/projects/ar-mobile-app.md",
         dateDeveloped: new Date(Date.parse("1 Oct 2018")),
+        githubLink: "https://github.com/dominicwild/HEART",
     },
     {
         image: `${cdnRootUrl}/microbit.jpg`,
         descriptionFile: "markdown/projects/microbit-mesh.md",
         dateDeveloped: new Date(Date.parse("1 Oct 2017")),
+        githubLink: "https://github.com/dominicwild/MicroBitMesh",
     },
     {
         image: `${cdnRootUrl}/nlp.png`,
         descriptionFile: "markdown/projects/nlp-language-classifier.md",
         dateDeveloped: new Date(Date.parse("1 Oct 2018")),
+        githubLink: "https://github.com/dominicwild/Java-Corpus-Language-Classifier",
     },
     {
         image: `${cdnRootUrl}/social2.gif`,
         descriptionFile: "markdown/projects/enterprise-social-network.md",
         dateDeveloped: new Date(Date.parse("1 Jan 2019")),
+        githubLink: "https://github.com/dominicwild/SocialNetwork",
     },
     {
         image: `${cdnRootUrl}/auction_system.gif`,
         descriptionFile: "markdown/projects/distributed-auction-system.md",
         dateDeveloped: new Date(Date.parse("1 Nov 2017")),
+        githubLink: "https://github.com/dominicwild/Auction-System",
     },
     {
         image: `${cdnRootUrl}/dht.png`,
         descriptionFile: "markdown/projects/distributed-storage-worker.md",
         dateDeveloped: new Date(Date.parse("1 Nov 2018")),
+        githubLink: "https://github.com/dominicwild/Distributed-Storage-Worker-System",
     }
 ].sort(
     (project, otherProject) => otherProject.dateDeveloped.getTime() - project.dateDeveloped.getTime()
